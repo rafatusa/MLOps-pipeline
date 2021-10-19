2e716c7153d6be34b08a240a70408319bc0e7c1a
Riduanul Islam <info.riduan@gmail.com>
2021-10-19 23:14:19 +0600

    added

--- temp.py
+++ temp.py
@@ -60,8 +60,8 @@ y_train = dataset.iloc[:, 1].values
 #print(dataset)
 
 # Splitting the dataset into the Training set and Test set
-# from sklearn.model_selection import train_test_split
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 dataset = pd.read_csv('test.csv')
 X_test = dataset.iloc[:, :-1].values
