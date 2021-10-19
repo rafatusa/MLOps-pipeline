fb0d9573e3353eeede0a89cd61c9e0e35d5b2059
Riduanul Islam <info.riduan@gmail.com>
2021-10-19 22:57:54 +0600

    added

--- temp.py
+++ temp.py
@@ -54,14 +54,14 @@ print(dataset)
 
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
+X_train = dataset.iloc[:, :-1].values
+y_train = dataset.iloc[:, 1].values
 
 #print(dataset)
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+# from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 dataset = pd.read_csv('test.csv')
 X_test = dataset.iloc[:, :-1].values
